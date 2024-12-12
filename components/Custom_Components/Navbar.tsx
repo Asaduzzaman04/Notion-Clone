e60e4e068e2579bf13d8e1eafe0e962bd62bf356
Asaduzzaman04 <asadchy152@gmail.com>
2024-12-12 14:23:54 +0600

    navbar created

--- components/Custom_Components/Navbar.tsx
+++ components/Custom_Components/Navbar.tsx
@@ -14,10 +14,12 @@ const Navbar = () => {
   return (
     <nav className="flex  justify-between items-center p-3 md:p-5 lg:px-[3%] bg-slate-800 ">
       {/* users-name */}
-      <div className="text-white font-bold text-2xl capitalize">{user ? <span>{user.user?.firstName}`s notes</span> : null}</div>
+      <div className="text-white font-bold text-2xl capitalize">
+        {user ? <span>{user.user?.firstName}`s notes</span> : null}
+      </div>
       {/* login-info */}
       <div>
-        <SignedOut >
+        <SignedOut>
           <SignInButton />
         </SignedOut>
 
